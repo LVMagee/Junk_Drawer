29068895166132238f0d8e5f625ed7156b25e2c6
LV Magee <lvmagee@gmail.com>
2017-06-11 12:06:17 -0400

    Add category schema insert

---
+++ seeders/categoryList.sql
@@ -0,0 +1,12 @@
+SELECT * FROM Assets_DB.Categories;
+
+INSERT INTO ASSETS_DB.categories (name) VALUES ("Appliances"); 
+INSERT INTO ASSETS_DB.categories (name) VALUES ("Antiques/Artworks");
+INSERT INTO ASSETS_DB.categories (name) VALUES ("Clothing/Bags");
+INSERT INTO ASSETS_DB.categories (name) VALUES ("Electronics");
+INSERT INTO ASSETS_DB.categories (name) VALUES ("Jewelry/Watches");
+INSERT INTO ASSETS_DB.categories (name) VALUES ("Tools");
+INSERT INTO ASSETS_DB.categories (name) VALUES ("Vehicles");
+INSERT INTO ASSETS_DB.categories (name) VALUES ("Other");
+
+
