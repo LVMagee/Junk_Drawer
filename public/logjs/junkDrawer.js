ec93142edb401092bd055151a4d6c74268bb7492
Tianna McHenry <tiannamchenry@tiannas-air.attlocal.net>
2017-06-13 13:44:45 -0400

    modified

--- public/logjs/junkDrawer.js
+++ public/logjs/junkDrawer.js
@@ -18,3 +18,18 @@ $(document).ready(function() {
 	 	// she
 	});
 });
+
+(function () {
+    var previous;
+
+    $("select").on('focus', function () {
+        // Store the current value on focus and on change
+        previous = this.value;
+    }).change(function() {
+        // Do something with the previous value after the change
+        alert(previous);
+
+        // Make sure the previous value is updated
+        previous = this.value;
+    });
+})();
