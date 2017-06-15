e3ca2f9717c219709bd53042220eaa21c334a06e
LV Magee <lvmagee@gmail.com>
2017-06-15 19:18:02 -0400

    Working on login

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,17 +5,37 @@
 // Dependencies
 // =============================================================
 var path = require("path");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
 
+ app.get("/", function(req, res) {
+    // If the owner already has an account send them to the junkDrawer page
+    if (req.owner) {
+      res.redirect("/home");
+    }
+    res.sendFile(path.join(__dirname + "/../views/index.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    // If the owner already has an account send them to the junkDrawer page
+    if (req.owner) {
+      res.redirect("/home");
+    }
+    res.sendFile(path.join(__dirname + "/../views/login.html"));
+  });
+
+
+
+
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname + "/../views/index.html"));
-  });
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname + "/../views/index.html"));
+  // });
 
   //once logged in
   app.get("/home", function(req, res) {
--- seeders/owner_seeds.sql
+++ seeders/owner_seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO owners (email, password) VALUES ("Sam Jones", "fruit123");
-INSERT INTO owners (email, password) VALUES ("Mary Smith", "7789123");
-INSERT INTO owners (email, password) VALUES ("Bobby Morgan", "today123");
+INSERT INTO owners (email, password) VALUES ("Samjones@email.com", "fruit123");
+INSERT INTO owners (email, password) VALUES ("Marysmith@earth.com", "7789123");
+INSERT INTO owners (email, password) VALUES ("Bobbymorgan@life.com", "today123");
