42492df3cc67e70b5a6b1bb826acdfd5cd9ae845
joshua wood <joshua wood>
2017-06-15 22:12:52 -0400

    completed insurance accordian

--- public/js/insuranceTable.js
+++ public/js/insuranceTable.js
@@ -25,47 +25,136 @@ $(document).ready(function(){
         rowsToAdd.push(createNewInsuranceRow(insurances[i]));
       }
 
-      insuranceContainer.append(rowsToAdd);
+      insuranceContainer.prepend(rowsToAdd);
   }
 
     // This function constructs a new insurance row for each insurance user has submitted
   function createNewInsuranceRow(insurance) {
 
-      var newItemRow = $("<tr>");
-      newItemRow.addClass("itemRowClick");
+      var x = Math.floor((Math.random() * 50000) + 1);
 
-      var newIndex = $("<td>");
-      newIndex.text(asset.id);
-      newItemRow.append(newIndex);
+      var newInsuranceRow = $("<div>");
+      newInsuranceRow.addClass("panel");
 
-      var newAsset = $("<td>");
-      newAsset.text(asset.itemName);
-      newItemRow.append(newAsset);
+      var newPanelHeading = $("<div>");
+      newPanelHeading.addClass("panel-heading");
+      newPanelHeading.attr("role", "tab");
+      newPanelHeading.attr("id", "headingOne" + x);
+      newInsuranceRow.append(newPanelHeading);
 
-      var newLinkRowContainer = $("<td>");
+      var newPanelHeadingTitle = $("<h4>");
+      newPanelHeadingTitle.addClass("panel-title");
+      newPanelHeading.append(newPanelHeadingTitle);
 
-      var newLinksRow = $("<div>");
-      newLinksRow.attr("id", "manageLinks")
-      newLinkRowContainer.append(newLinksRow);
+      var newPanelHeadingTitleText = $("<a>");
+      newPanelHeadingTitleText.attr("role", "button");
+      newPanelHeadingTitleText.attr("data-toggle", "collapse");
+      newPanelHeadingTitleText.attr("data-parent", "#accordian");
+      newPanelHeadingTitleText.attr("href", "#collapseOne" + x);
+      newPanelHeadingTitleText.attr("aria-expanded", "true");
+      newPanelHeadingTitleText.attr("aria-controls", "collapseOne" + x);
+      newPanelHeadingTitleText.text(insurance.company);
+      newPanelHeadingTitle.append(newPanelHeadingTitleText);
 
-      var editIcon = $("<span>");
-      editIcon.addClass("glyphicon glyphicon-pencil itemEdit");
-      editIcon.attr("aria-hidden", "true");
-      newLinksRow.append(editIcon);
+      var deleteInsuranceIcon = $("<span>");
+      deleteInsuranceIcon.addClass("glyphicon glyphicon-remove insuranceRemove");
+      deleteInsuranceIcon.data("id", insurance.id);
+      newPanelHeadingTitleText.append(deleteInsuranceIcon);
 
-      var deleteIcon = $("<span>");
-      deleteIcon.addClass("glyphicon glyphicon-remove itemRemove");
-      deleteIcon.data("id", asset.id);
-      newLinksRow.append(deleteIcon);
+      var newCollapsePanel = $("<div>");
+      newCollapsePanel.attr("id", "collapseOne" + x);
+      newCollapsePanel.addClass("panel-collapse collapse in");
+      newCollapsePanel.attr("role", "tabpanel");
+      newCollapsePanel.attr("aria-labelledby", "headingOne" + x);
+      newInsuranceRow.append(newCollapsePanel);
 
-      newItemRow.append(newLinkRowContainer);
+      var newPanelBody = $("<div>");
+      newPanelBody.addClass("panel-body");
+      newCollapsePanel.append(newPanelBody);
 
-      return newItemRow;
+      var newDiv2 = $("<div>");
+      newPanelBody.append(newDiv2);
+
+      var newCenter = $("<center>");
+      newDiv2.append(newCenter);
+
+      var insuranceType = $("<h4>");
+      insuranceType.text("Insurance Type");
+      var insuranceTypeText = $("<p>");
+      insuranceTypeText.text(insurance.insur_type);
+      insuranceType.append(insuranceTypeText);
+      newCenter.append(insuranceType);
+
+      var break1 = $("<br>");
+      newCenter.append(break1);
+
+      var companyName = $("<h4>");
+      companyName.text("Company Name");
+      var companyNameText = $("<p>");
+      companyNameText.text(insurance.company);
+      companyName.append(companyNameText);
+      newCenter.append(companyName);
+
+      var break2 = $("<br>");
+      newCenter.append(break2);
+
+      var policyNumber = $("<h4>");
+      policyNumber.text("Policy Number");
+      var policyNumberText = $("<p>");
+      policyNumberText.text(insurance.policy_num);
+      policyNumber.append(policyNumberText);
+      newCenter.append(policyNumber);
+
+      var break3 = $("<br>");
+      newCenter.append(break3);
+
+      var startingDate = $("<h4>");
+      startingDate.text("Start Date");
+      var startDateText = $("<p>");
+      startDateText.text(insurance.start_date);
+      startingDate.append(startDateText);
+      newCenter.append(startingDate);
+
+      var break4 = $("<br>");
+      newCenter.append(break4);
+
+      var phoneNumber = $("<h4>");
+      phoneNumber.text("Phone Number");
+      var phoneNumberText = $("<p>");
+      phoneNumberText.text(insurance.phone_num);
+      phoneNumber.append(phoneNumberText);
+      newCenter.append(phoneNumber);
+
+      var break5 = $("<br>");
+      newCenter.append(break5);
+
+      var newAgent = $("<h4>");
+      newAgent.text("Agent");
+      var newAgentText = $("<p>");
+      newAgentText.text(insurance.agent);
+      newAgent.append(newAgentText);
+      newCenter.append(newAgent);
+
+      var break6 = $("<br>");
+      newCenter.append(break6);
+
+      var newAddInfo = $("<h4>");
+      newAddInfo.text("Additional Comments");
+      var newAddInfoText = $("<p>");
+      newAddInfoText.text(insurance.info);
+      newAddInfo.append(newAddInfoText);
+      newCenter.append(newAddInfo);
+
+      var break7 = $("<br>");
+      newCenter.append(break7);
+
+      var editInsuranceIcon = $("<span>");
+      editInsuranceIcon.addClass("glyphicon glyphicon-pencil insuranceEdit");
+      editInsuranceIcon.attr("aria-hidden", "true");
+      newCenter.append(editInsuranceIcon);
+
+      return newInsuranceRow;
   }
-
-  //code to display full insurance info when insurance row is clicked
-  //=================================================================
-  //=================================================================
 
   //Code to submit new insurance to database via insurance form
   //======================================================================
