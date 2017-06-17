9709e1a86b280793471b2baf35dd7d27ae6fd9d0
LV Magee <lvmagee@gmail.com>
2017-06-17 01:47:44 -0400

    Fixed the html to work with login, again

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -66,38 +66,3 @@ module.exports = function(app) {
   });
 };
 
-
-
-
-
-
-// module.exports = function(app){
-
-// 	//POST route for adding new user
-// 	app.post("/api/owner", function(req,res){
-// 		db.Owner.create({
-
-// 			email: req.body.email,
-// 			password: req.body.password
-
-// 		}).then(function(owner){
-			
-// 			res.json(owner);
-// 		});
-// 	});
-	
-	// //route to UPDATE asset
-	// app.put("/api/owner", function(req,res){
-	// 	db.Owner.update(
-	// 	{
-	// 		email: req.body.email,
-	// 		password: req.body.password,
-		
-	// 	},{
-	// 		where: {id: req.body.id}
-	// 	}).then(function(owner){
-	// 		res.json(owner);
-	// 	});
-	// });
-
-// }
