976670de9d275de3384c378d47b2f699d7c717a7
joshua wood <joshua wood>
2017-06-17 12:54:44 -0400

    .

--- seeders/user_seeds.js
+++ seeders/user_seeds.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.JAWSDB_URL);
 
 
-sequelize.query('INSERT INTO User (users email, password) VALUES ("Samjones@email.com", "fruit123")');
-sequelize.query('INSERT INTO User (users email, password) VALUES ("Marysmith@earth.com", "7789123")');
-sequelize.query('INSERT INTO User (users email, password) VALUES ("Bobbymorgan@life.com", "today123")');
+sequelize.query('INSERT INTO User (email, password) VALUES ("Samjones@email.com", "fruit123")');
+sequelize.query('INSERT INTO User (email, password) VALUES ("Marysmith@earth.com", "7789123")');
+sequelize.query('INSERT INTO User (email, password) VALUES ("Bobbymorgan@life.com", "today123")');
 
