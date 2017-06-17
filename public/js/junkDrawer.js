00bc84f390e8f0cba638e460ccc7b00b31b4f340
joshua wood <joshua wood>
2017-06-16 23:21:16 -0400

    ...

--- public/js/junkDrawer.js
+++ public/js/junkDrawer.js
@@ -2,20 +2,10 @@ $(document).ready(function(){
 	var nav = document.getElementById("navBar")
 
 	$(document).on('click','.itemRowClick',function(){
+        $("#itemDetail").toggle("blind", 1000);
+        nav.style.opacity = 0.25;
+        $("#displayItemName").empty();
 
-		document.getElementById("icons").onClick = function(){
-
-			var deleteClick = document.getElementById("icons");
-
-			if (deleteClick.clicked = true){
-
-			}else{
-				
-				$("#itemDetail").toggle("blind", 1000);
-				nav.style.opacity = 0.25;
-				$("#displayItemName").empty();
-			}
-		}
 	});
 
 	$("#closeItemDetail").click(function(){
