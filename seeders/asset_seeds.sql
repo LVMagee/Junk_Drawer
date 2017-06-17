ef39dfb7f59a47c4c75cbd84cb059b3fc55654c7
Joshua Wood <jwood75@uncc.edu>
2017-06-17 10:22:57 -0400

    Update asset_seeds.sql

--- seeders/asset_seeds.sql
+++ seeders/asset_seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO assets ( itemName, category, make, model, serial_num, bought, price, info, UserId) VALUES ("Freezer", "Appliances", "GE", "Frost Free LS", "RT-6789", "2016,11,12", "1200.23", "Has a 3 year warranty. Service 800-555-5656", 1);
-INSERT INTO assets ( itemName, category, make, model, serial_num, bought, price, info, UserId) VALUES ("Lisa's Laptop", "Electronics", "Apple", "MacBook Pro", "03456-78-0012", "2017,02,18", "1890.23", "Has a 2 year warranty. Service 800-555-5656", 1);
-INSERT INTO assets ( itemName, category, make, model, serial_num, bought, price, info, UserId) VALUES ("Painting", "Antiques/Artworks", "Van Gogh", "Starry Night", "1", "1817,02,18", "1100890.23", "Stole it from MOMA, but no one has noticed the forgery.", 1);
+INSERT INTO assets ( itemName, category, make, model, serial_num, bought, price, info) VALUES ("Freezer", "Appliances", "GE", "Frost Free LS", "RT-6789", "2016,11,12", "1200.23", "Has a 3 year warranty. Service 800-555-5656");
+INSERT INTO assets ( itemName, category, make, model, serial_num, bought, price, info) VALUES ("Lisa's Laptop", "Electronics", "Apple", "MacBook Pro", "03456-78-0012", "2017,02,18", "1890.23", "Has a 2 year warranty. Service 800-555-5656");
+INSERT INTO assets ( itemName, category, make, model, serial_num, bought, price, info) VALUES ("Painting", "Antiques/Artworks", "Van Gogh", "Starry Night", "1", "1817,02,18", "1100890.23", "Stole it from MOMA, but no one has noticed the forgery.");
 
 
