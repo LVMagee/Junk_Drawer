fbb8320767a7381ec2259faaf11a4d00c384977d
Joshua Wood <jwood75@uncc.edu>
2017-06-17 10:18:43 -0400

    Update associations.js

--- associations.js
+++ associations.js
@@ -1,6 +1,6 @@
-module.exports = function(db) {
-  db.Asset.belongsTo(db.User, { foreignKey: { allowNull: false } });
-  db.Insurance.belongsTo(db.User, { foreignKey: { allowNull: false } });
-  db.User.hasMany(db.Asset, { onDelete: "cascade" });
-  db.User.hasMany(db.Insurance, { onDelete: "cascade" });
-};
+// module.exports = function(db) {
+//   db.Asset.belongsTo(db.User, { foreignKey: { allowNull: false } });
+//   db.Insurance.belongsTo(db.User, { foreignKey: { allowNull: false } });
+//   db.User.hasMany(db.Asset, { onDelete: "cascade" });
+//   db.User.hasMany(db.Insurance, { onDelete: "cascade" });
+// };
