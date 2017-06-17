bfe365c780b936fc07c9cfd878e56258ae6758d3
joshua wood <joshua wood>
2017-06-17 12:49:38 -0400

    .

--- seeders/insure_seeds.js
+++ seeders/insure_seeds.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.JAWSDB_URL);
 
-sequelize.query('INSERT INTO  insurances (insur_type, company, phone_num, agent, policy_num, start_date, info) VALUES ("Home", "Allstate", "800-555-1234", "Joe Smo", "p# 568902", "2010,03,04", "Need to review all claims made before selling house")');
-sequelize.query('INSERT INTO  insurances (insur_type, company, phone_num, agent, policy_num, start_date, info) VALUES ("Vehicle", "Geico", "800-555-1234", "Joe Smo", "p# 568902", "2010,03,10", "Remove older vehivle from policy")');
+sequelize.query('INSERT INTO  Insurances (insur_type, company, phone_num, agent, policy_num, start_date, info) VALUES ("Home", "Allstate", "800-555-1234", "Joe Smo", "p# 568902", "2010,03,04", "Need to review all claims made before selling house")');
+sequelize.query('INSERT INTO  Insurances (insur_type, company, phone_num, agent, policy_num, start_date, info) VALUES ("Vehicle", "Geico", "800-555-1234", "Joe Smo", "p# 568902", "2010,03,10", "Remove older vehivle from policy")');
 
 // INSERT INTO  insurances (insur_type, company, phone_num, agent, policy_num, start_date, info) VALUES ("Home", "Allstate", "800-555-1234", "Joe Smo", "p# 568902", "2010,03,04", "Need to review all claims made before selling house");
 
--- seeders/user_seeds.js
+++ seeders/user_seeds.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.JAWSDB_URL);
 
-sequelize.query('INSERT INTO (users email, password) VALUES ("Samjones@email.com", "fruit123")')
-sequelize.query('INSERT INTO (users email, password) VALUES ("Marysmith@earth.com", "7789123")')
-sequelize.query('INSERT INTO (users email, password) VALUES ("Bobbymorgan@life.com", "today123")')
+
+sequelize.query('INSERT INTO User (users email, password) VALUES ("Samjones@email.com", "fruit123")');
+sequelize.query('INSERT INTO User (users email, password) VALUES ("Marysmith@earth.com", "7789123")');
+sequelize.query('INSERT INTO User (users email, password) VALUES ("Bobbymorgan@life.com", "today123")');
 
